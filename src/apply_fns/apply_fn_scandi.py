430d8407ace4a4a4b80108d20e9941634e076eba
MinaAlmasi <mina.almasi00@gmail.com>
2023-02-14 13:14:50 +0100

    dacy wrap of scandiner

--- src/apply_fns/apply_fn_scandi.py
+++ src/apply_fns/apply_fn_scandi.py
@@ -4,10 +4,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 from transformers import pipeline
 import pandas as pd
 
-scandi_ner = pipeline(task='ner', model='saattrupdan/nbailab-base-ner-scandi',  aggregation_strategy='first', device = 1, framework = "pt")
+#scandi_ner = pipeline(task='ner', model='saattrupdan/nbailab-base-ner-scandi',  aggregation_strategy='first', device = 1, framework = "pt")
 
 # load ScandiNER (https://huggingface.co/saattrupdan/nbailab-base-ner-scandi) using Dacy (https://centre-for-humanities-computing.github.io/DaCy/using_dacy.getting_started.html#named-entity-recognition)
-#import spacy, dacy
+import spacy, dacy
 
-#scandi_ner = spacy.blank("da")
-#scandi_ner.add_pipe("dacy/ner")
+scandi_ner = spacy.blank("da")
+scandi_ner.add_pipe("dacy/ner")
