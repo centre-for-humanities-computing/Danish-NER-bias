f96ed8be9c7a3d3571d846b8f031956c47a89cd9
MinaAlmasi <minaalmasi00@gmail.com>
2023-02-14 11:39:34 +0100

    overlap checked

--- src/helper_fns/process_data/overlap.py
+++ src/helper_fns/process_data/overlap.py
@@ -11,11 +11,56 @@ men_2023["Navn"] = men_2023["Navn"].str.title()
 women_2023["Navn"] = women_2023["Navn"].str.title()
 
 # first names from old and new list 
-men_old_names = list(m_name_dict["first_name"])
-men_2023_names = list(men_2023["Navn"])
+men_old_names = m_name_dict["first_name"]
+all_men_2023_names = list(men_2023["Navn"])
 
-# find overlap
-men_overlap = list(set(men_old_names).intersection(men_2023_names))
-print(men_overlap)
-print(f"Overlap between men lists: {len(men_overlap)}")
-print(f"Len of OG men names list: {len(men_old_names)}")
+women_old_names = f_name_dict["first_name"]
+all_women_2023_names = list(women_2023["Navn"])
+
+# define function to find overlap
+def find_overlap(lst1, lst2):
+    lst1 = set(lst1)
+    lst2 = set(lst2)
+
+    overlap = list(lst1.intersection(lst2))
+    len_overlap = len(overlap)
+
+    return len_overlap, overlap
+
+# overlaps for entire men names 2023 
+men_all_overlap = find_overlap(men_old_names, all_men_2023_names)
+
+# subset men 2023
+subset_men_2023_names = all_men_2023_names[:len(men_old_names)]
+
+men_subset_overlap = find_overlap(men_old_names, subset_men_2023_names)
+
+# overlaps for entire women names 2023 
+women_all_overlap = find_overlap(women_old_names, all_women_2023_names)
+
+# subset women 2023
+subset_women_2023_names = all_women_2023_names[:len(women_old_names)]
+
+women_subset_overlap = find_overlap(women_old_names, subset_women_2023_names)
+
+## all overlaps 
+overlaps = {
+    "Overlap list": ["all 2023 names", "2023 subset popular names"],
+    "Men overlaps" : [men_all_overlap[0], men_subset_overlap[0]],
+    "Women overlaps": [women_all_overlap[0], women_subset_overlap[0]],
+}
+print("\n")
+
+print(pd.DataFrame(overlaps))
+
+lengths = {
+    "Lists": ["all m/w 2023 names", "2023 subset popular", "old names"],
+    "Women (Len)": [len(all_women_2023_names), len(subset_women_2023_names), len(women_old_names)],
+    "Men (Len)": [len(all_men_2023_names), len(subset_men_2023_names), len(men_old_names)]
+}
+
+print("\n")
+
+print(pd.DataFrame(lengths))
+
+print("\n")
