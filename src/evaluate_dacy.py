362912590aabccb8d2c43fcc594630463f84032e
MinaAlmasi <minaalmasi00@gmail.com>
2023-03-23 18:02:34 +0100

    fix dacy

--- src/evaluate_dacy.py
+++ src/evaluate_dacy.py
@@ -1,17 +1,23 @@
-#import ssl certificate to download spacy models
+# import ssl certificate to download models
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 # import packages
 import spacy
-import dacy 
+import dacy
+
+# add custom modules 
+import sys
+from pathlib import Path
+module_path = Path(__file__).parents[0] / "evaluate_fns"
+sys.path.append(module_path)
 
 # Dataset
 from dacy.datasets import dane
 testdata = dane(splits=["test"], redownload=True, open_unverified_connected=True)
 
 ### Define augmenters ###
-from helper_fns.augmentation import dk_aug, muslim_aug, f_aug, m_aug, muslim_f_aug, muslim_m_aug, unisex_aug
+from evaluate_fns.augmentation import dk_aug, muslim_aug, f_aug, m_aug, muslim_f_aug, muslim_m_aug, unisex_aug
 
 n = 20
 # augmenter, name, n repetitions 
@@ -27,12 +33,12 @@ augmenters = [
 
 ### Define Models to Run ###
 model_dict = {
-    "dacy_small": "da_dacy_small_trf-0.1.0",
-    "dacy_medium": "da_dacy_medium_trf-0.1.0",
+    #"dacy_small": "da_dacy_small_trf-0.1.0",
+    #"dacy_medium": "da_dacy_medium_trf-0.1.0",
     "dacy_large": "da_dacy_large_trf-0.1.0"
 }
 
 ### Performance ###
-from helper_fns.performance import eval_model_augmentation 
+from evaluate_fns.performance import eval_model_augmentation 
 
 eval_model_augmentation(model_dict, augmenters, testdata)
